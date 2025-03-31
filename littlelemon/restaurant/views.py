d82b44c502b0e9e5c30576a53ea5e5143b4c296b
Mackroph <205552813+CristianMz21@users.noreply.github.com>
2025-03-31 12:01:16 -0500

    add auth

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'restaurant/index.html', {})
+
+@api_view()
+@permission_classes([IsAuthenticated])
+def msg(request):
+    return Response({"message":"This view is protected"})
 
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
@@ -16,6 +24,7 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
     serializer_class = MenuSerializer
 
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
 
